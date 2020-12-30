acd8eb09c2150bfe17c98167c032608d51908c9b
Ludovic <58370115+Rambovic45@users.noreply.github.com>
2020-12-30 13:34:02 -0800

    jsp

--- bin/start.bat
+++ bin/start.bat
@@ -1,5 +1,5 @@
 @echo off 
 title PingShell
 cls
-::Start a gui module
-@powershell.exe -noprofile -executionpolicy bypass -file "%~dp0./script/gui.ps1"
+::Start a main.ps1 module
+@powershell.exe -noprofile -executionpolicy bypass -file "%~dp0./script/main.ps1"
